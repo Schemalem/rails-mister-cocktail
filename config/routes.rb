526793234d7883f1bd937d5720bb1cb80dc82d34
Nicole Schembri <nschembri93@gmail.com>
2021-01-30 17:00:34 +0000

    Structure all set up for doses and cocktails, ready for design

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [ :index, :show, :new, :create ] do
-      resources :doses, only: [ :new, :create ]
-    end
+    resources :doses, only: [ :new, :create ]
+  end
   resources :doses, only: [ :destroy ]
-  root to: 'cocktails#home'
+  # delete "doses/:id", to: 'doses#destroy'
+  root to: 'cocktails#index'
 end
